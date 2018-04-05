b14b2cbe7e666248b3d94471c4bba604679114c5
Mesquita, Ruy <ruy.mesquita@getninjas.com.br>
2018-04-05 00:01:02 -0300

    minor refactor

--- app/src/main/java/br/rpm/call/list/model/CallDetail.kt
+++ app/src/main/java/br/rpm/call/list/model/CallDetail.kt
@@ -1,22 +1,9 @@
 package br.rpm.call.list.model
 
 class CallDetail {
-
     var date: String? = null
     var duration: String? = null
     var callType: String? = null
     var number: String? = null
     var name: String? = null
-
-    constructor() {
-
-    }
-
-    constructor(timestamp: String, duration: String, callType: String, number: String, name: String) {
-        this.date = timestamp
-        this.duration = duration
-        this.callType = callType
-        this.number = number
-        this.name = name
-    }
 }
